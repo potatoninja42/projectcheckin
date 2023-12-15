5c0b03a78bb72d947bb3ce0a02f00ac46a73cdab
potatoninja42 <152187253+potatoninja42@users.noreply.github.com>
2023-12-14 20:02:08 -0500

    Update 1st attempt.py
    grab data from pdf

--- 1st attempt.py
+++ 1st attempt.py
@@ -1,24 +1,8 @@
-# Read all employee data from the file
-employee_data = []
-with open("employee_data.txt", "r") as file:
-    employee_data = file.readlines()
+from pdfminer.high_level import extract_text
+import pandas as pd
 
-# Organize the data by date and number of packages delivered
-organized_data = {}
-for line in employee_data:
-    full_name, employee_id, packages_delivered, date = line.strip().split(',')
-    key = (date, int(packages_delivered))
-    
-    if key not in organized_data:
-        organized_data[key] = []
-    
-    organized_data[key].append(line)
+def extract_pdf_data():
+    text = extract_text()
+    lines = text.split('\n')
 
-# Create a new text file to store the organized data
-organized_filename = "organized_employee_data.txt"
-with open(organized_filename, "w") as file:
-    for key in sorted(organized_data.keys()):
-        for line in organized_data[key]:
-            file.write(line)
-
-print("Additional employee data has been added and organized in 'organized_employee_data.txt'.")
+    data = []
